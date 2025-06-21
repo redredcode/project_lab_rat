2b6d5a4aed54ce1c1706a9410e479b938b75a466
fahim <thebigredbhuiyan@gmail.com>
2025-06-21 18:19:55 +0600

    chore: add stripe package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:project_lab_rat/app.dart';
 
 import 'package:project_lab_rat/main.dart';
 
